f0d3e7dab857c9dde66f7e09db7915caf076baa0
abhijithkmpixel <88177542+abhijithkmpixel@users.noreply.github.com>
2022-05-16 15:03:00 +0530

    df

---
+++ public/vivi/wp-content/plugins/flow-flow/libs/composer/installed.php
@@ -0,0 +1,98 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4b91fa702ca43e05ad45bddf062e039095c79186',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4b91fa702ca43e05ad45bddf062e039095c79186',
+    ),
+    'cakephp/cache' => 
+    array (
+      'pretty_version' => '3.9.0',
+      'version' => '3.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e8ec4e77fb288adda318e08053f5f540870aeb9d',
+    ),
+    'cakephp/core' => 
+    array (
+      'pretty_version' => '3.x-dev',
+      'version' => '3.9999999.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'eadccd407141ea8ef8afac5e01467110543d6484',
+    ),
+    'cakephp/utility' => 
+    array (
+      'pretty_version' => '3.x-dev',
+      'version' => '3.9999999.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '51b0af31af3239f6141006bbd7cbc7b16aba40d6',
+    ),
+    'colshrapnel/safemysql' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'e64dacc23247b784595aa09d08f8683dc8982eb5',
+    ),
+    'flowflow/core' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => 'a51dd40e6f15ca191bd64f0a520a99d42c9df7f0',
+    ),
+    'flowflow/social' => 
+    array (
+      'pretty_version' => 'dev-remove_open_api',
+      'version' => 'dev-remove_open_api',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7a63041d1d19505323c1ec8283e19d29b8f10f49',
+    ),
+    'mashape/unirest-php' => 
+    array (
+      'pretty_version' => 'v3.0.4',
+      'version' => '3.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '842c0f242dfaaf85f16b72e217bf7f7c19ab12cb',
+    ),
+    'psr/simple-cache' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '408d5eafb83c57f6365a3ca330ff23aa4a5fa39b',
+    ),
+  ),
+);
